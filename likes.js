ec7cd9a61913ee3a57fbb28673126bde5df2d0bd
Jacob Roe <jroenavy24@gmail.com>
2021-04-27 15:47:52 -0700

    new commit

--- likes.js
+++ likes.js
@@ -1,8 +1,24 @@
-var count = 0
-function likeAction(){
-    let myElement = document.querySelector("#like_count");
+var count1 = 0
+function likeAction1(){
+    let myElement = document.querySelector("#like_count1");
     console.log("You got a like!", myElement);
-    count++;
-    myElement.innerHTML = count;
-    console.log(count);
+    count1++;
+    myElement.innerHTML = count1;
+    console.log(count1);
+}
+var count2 = 0
+function likeAction2(){
+    let myElement = document.querySelector("#like_count2");
+    console.log("You got a like!", myElement);
+    count2++;
+    myElement.innerHTML = count2;
+    console.log(count2);
+}
+var count3 = 0
+function likeAction3(){
+    let myElement = document.querySelector("#like_count3");
+    console.log("You got a like!", myElement);
+    count3++;
+    myElement.innerHTML = count3;
+    console.log(count3);
 }
